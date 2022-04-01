35447d9a07ff272443eab1e168b8496f27430a70
finnp920 <finnp920@gmail.com>
2022-04-01 21:39:41 +0900

    [B] change Dashboard layout

--- vue/src/pages/Work.vue
+++ vue/src/pages/Work.vue
@@ -3,23 +3,21 @@
     <div class="page-top">
       <h3 class="pathname">{{ pagePathName }}</h3>
       <div class="button-wrap">
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
+        <button style="width: auto; padding: 0 10px; background-color: #ffffff; font-size: 18px" @click="setNumOfColumns(1)">↻</button>
+        <button @click="setNumOfColumns(2)">2</button>
+        <button @click="setNumOfColumns(3)">3</button>
+        <button @click="setNumOfColumns(4)">4</button>
       </div>
     </div>
-    <div class="box-wrap">
-      <div
-        v-for="(item, idx) in box.items"
-        :key="'box' + idx"
-        class="box"
-      >
+    <div class="box-wrap" :style="{ gridTemplateColumns: `repeat(${numOfColumns}, 1fr)` }">
+      <div v-for="(item, idx) in box.items" :key="`box${idx}`" class="box">
         <p>{{ item.name }}</p>
       </div>
     </div>
   </div>
 </template>
 <script>
+import { mapGetters, mapMutations } from 'vuex';
 import 'scss/pages/work.scss';
 
 export default {
@@ -29,13 +27,30 @@ export default {
       type: Object,
       default() {
         return this.$store.state.ui.box;
-      }
-    }
+      },
+    },
+  },
+  data() {
+    return {};
   },
   computed: {
+    ...mapGetters({
+      numOfColumns: 'ui/getNumOfColumns',
+    }),
     pagePathName() {
-      return 'page path: ' + this.$route.path;
-    }
-  }
+      return `page path: ${this.$route.path}`;
+    },
+  },
+  methods: {
+    ...mapMutations({ setNumOfColumns: 'ui/setNumOfColumns' }),
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.box-wrap {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0 8px;
+}
+</style>
--- vue/src/store/modules/ui.js
+++ vue/src/store/modules/ui.js
@@ -2,21 +2,30 @@ const state = {
   box: {
     items: Array.apply(null, new Array(12)).map((item, idx) => {
       return {
-        name: 'box - ' + (idx + 1)
+        name: `box - ${idx + 1}`,
       };
     }),
-    numOfColumns: 1
-  }
+    numOfColumns: 1,
+  },
 };
 
-const getters = {};
+const getters = {
+  getNumOfColumns: function (state) {
+    return state.box.numOfColumns;
+  },
+};
 const actions = {};
-const mutations = {};
+const mutations = {
+  setNumOfColumns(state, value) {
+    if (typeof value !== 'number' || value < 1) return;
+    state.box.numOfColumns = value;
+  },
+};
 
 export default {
   namespaced: true,
   state,
   getters,
   actions,
-  mutations
+  mutations,
 };
